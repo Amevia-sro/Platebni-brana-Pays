46d5d515f63f00966172e33406069863eb43976d
Amevia <66975640+Amevia-sro@users.noreply.github.com>
2021-10-24 16:41:21 +0200

    Update __manifest__.py

--- payment_pays/__manifest__.py
+++ payment_pays/__manifest__.py
@@ -4,6 +4,7 @@
     'name': 'Pays Payment Gateway',
     'version': '1.0',
     'summary': 'Payment Gateway integration for Czech Republic',
+    'author': 'Amevia s.r.o.',
     'description': """""",
     'category': 'Payment Gateway: Pays Implementation',
     'depends': ['payment'],
